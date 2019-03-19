d5efe929a07a262d4630377c85a1ae0c94ab05e9
github username <github email address>
2019-03-19 08:47:40 -0500

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+resources :posts
+#patch '/posts/id/:edit', to: 'posts#update', as: "update_post"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -17,7 +16,7 @@ ActiveRecord::Schema.define(version: 20160113011820) do
     t.string   "name"
     t.string   "genre"
     t.text     "bio"
-    t.datetime "created_at", null: false
+    t.datetime "created_at", null: false 
     t.datetime "updated_at", null: false
   end
 
